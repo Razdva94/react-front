787b8cecca5853addbaf4f53ecfe2429dd64e875
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-11-06 12:35:28 +0300

    change slide

--- src/copmonents/LateralSlide/LateralSlide.jsx
+++ src/copmonents/LateralSlide/LateralSlide.jsx
@@ -4,30 +4,32 @@ import BenelliIcon from '../../images/benelli_icon.png';
 
 const LateralSlide = ({ handleNavOpen, navOpen }) => {
   return (
-    <nav className={`lateral-slide ${!navOpen && 'slide-out'}`}>
-      <img class='lateral-slide__icon' src={BenelliIcon} alt='logo' />
-      <a
-        className='lateral-slide__text lateral-slide__text_margin'
-        href='/'
-        onClick={handleNavOpen}
-      >
-        Главная
-      </a>
-      <a
-        className='lateral-slide__text lateral-slide__text_margin'
-        href='/#motorcycles'
-        onClick={handleNavOpen}
-      >
-        Мотоциклы
-      </a>
-      <a
-        className='lateral-slide__text lateral-slide__text_margin'
-        href='#footer'
-        onClick={handleNavOpen}
-      >
-        Контакты
-      </a>
-    </nav>
+    <div className={`${navOpen && 'lateral-slide__container'}`}>
+      <nav className={`lateral-slide ${!navOpen && 'slide-out'}`}>
+        <img class='lateral-slide__icon' src={BenelliIcon} alt='logo' />
+        <a
+          className='lateral-slide__text lateral-slide__text_margin'
+          href='/'
+          onClick={handleNavOpen}
+        >
+          Главная
+        </a>
+        <a
+          className='lateral-slide__text lateral-slide__text_margin'
+          href='/#motorcycles'
+          onClick={handleNavOpen}
+        >
+          Мотоциклы
+        </a>
+        <a
+          className='lateral-slide__text lateral-slide__text_margin'
+          href='#footer'
+          onClick={handleNavOpen}
+        >
+          Контакты
+        </a>
+      </nav>
+    </div>
   );
 };
 
